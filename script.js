ac9b9ff016ac937f5b3e6962740b189eba287a5f
Kayashsa <maximkorejnevskiy@gmail.com>
2020-11-26 16:30:50 +0300

    Demo Message

---
+++ script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> XXX </title>
+  <script>
+    // JavaScript-код
+  </script>
+</head>
+<body>
+  <script>
+    // JavaScript-код
+  </script>
+</body>
+</html>
