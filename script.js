4457095da5f56726955f5c4fdf4f4c692cbb77a3
Kayashsa <maximkorejnevskiy@gmail.com>
2020-12-06 21:34:59 +0300

    OnelineJS

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title> XXX </title>
-  <script>
-    // JavaScript-код
-  </script>
-</head>
-<body>
-  <script>
-    // JavaScript-код
-  </script>
-</body>
-</html>
+  alert("hello world")
